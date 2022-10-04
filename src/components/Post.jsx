88fa4c8055660a9072821902fffb8e4421acfea9
Emanuel <emanuelssobral@gmail.com>
2022-10-04 00:10:13 -0300

    Avatar Module

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
@@ -6,7 +7,7 @@ export function Post() {
         <article className={styles.post}>
             <header className={styles.headerPost}>
                 <div className={styles.author}>
-                    <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/82630009?v=4" />
+                    <Avatar src="https://avatars.githubusercontent.com/u/82630009?v=4" />
                     <div className={styles.authorInfo}>
                         <strong>Emanuel Souza</strong>
                         <span>Web Developer</span>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { Pen } from 'phosphor-react';
+import { Avatar } from './Avatar';
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -10,7 +11,7 @@ export function Sidebar() {
              />
 
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/82630009?v=4"/>
+                <Avatar src = "https://avatars.githubusercontent.com/u/82630009?v=4"/>
 
                 <strong>Emanuel Souza</strong>
                 <span>Web Developer</span>
